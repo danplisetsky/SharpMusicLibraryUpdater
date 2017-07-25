3c02614f678c9f7eaea2cc2a5d9fa98d4f0afaf7
Daniel Plisetsky <danplisetsky@gmail.com>
2017-07-25 16:41:30 +0300

    Add 'New Albums Available' column

--- SharpMusicLibraryUpdater.App/Resources/Converters.cs
+++ SharpMusicLibraryUpdater.App/Resources/Converters.cs
@@ -41,4 +41,24 @@ namespace SharpMusicLibraryUpdater.App.Resources
         }
         public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture) => new[] { DependencyProperty.UnsetValue };
     }
+
+    public class IsIgnoredAndNewAlbumsToStatusConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values[0] is bool isIgnored && isIgnored)
+                return String.Empty;
+            if (values[1] is List<NewAlbum> newAlbums)
+            {
+                int count = newAlbums.Count(al => !al.MarkAsSeen);
+                return count == 0
+                    ? "No new albums available"
+                    : count == 1
+                        ? "1 new album available"
+                        : $"{count} new albums available";
+            }
+            return DependencyProperty.UnsetValue;
+        }
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture) => new[] { DependencyProperty.UnsetValue };
+    }
 }
